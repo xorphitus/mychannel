a52fa37c1904d58b391f8151e22d5e9cd7299ae3
T. Matsu <xorphitus@gmail.com>
2012-09-05 22:47:19 +0900

    voices_controllerをリファクタ エラーケースへの対応と、TODOの付与

--- app/controllers/voices_controller.rb
+++ app/controllers/voices_controller.rb
@@ -1,26 +1,44 @@
 # -*- coding: utf-8 -*-
-require 'net/http'
 
 class VoicesController < ApplicationController
   include Authentication
 
   def emit
     text = params[:text].gsub(/\s+/, ',').gsub(/[a-zA-Z]+/) {|m| m.to_katakana}
+
     if text.length > 100 then
       # TODO できるだけ最後の句読点で切る
-      text = URI.encode (text[0, 97] + '以下略')
+      text = text[0, 97] + '以下略'
     end
 
-    voice = get_voice(text)
-    if voice.length == 0 then
-      logger.debug 'failed to get voice data!'
-      logger.debug text
-      voice = get_voice('音声に変換できない文章が設定されました。ごめんなさい')
-    end
+    voice = get_voice text
     send_data voice, :type => 'audio/mpeg'
   end
 
-  def get_voice(text)
-    Net::HTTP.get 'translate.google.co.jp', "/translate_tts?tl=ja&ie=utf-8&q=#{text}"
+  # TODO アクセシビリティを下げる？
+  def get_voice text
+    begin
+      encoded = URI.encode text
+      uri = URI.parse "http://translate.google.co.jp/translate_tts?tl=ja&ie=utf-8&q=#{encoded}"
+    rescue URI::InvalidURIError
+      return get_voice_on_fail text
+    end
+
+    voice = Net::HTTP.get uri
+    if voice.length == 0 then
+      voice = get_voice_on_fail text
+    end
+
+    return voice
+  end
+
+  # TODO アクセシビリティを下げる？
+  def get_voice_on_fail text
+    if text.nil? then
+      # TODO herokuでは logger.debug 使用不可
+      logger.debug 'failed to get voice data!'
+      logger.debug text
+    end
+    get_voice '音声に変換できない文章が設定されました。ごめんなさい'
   end
 end
