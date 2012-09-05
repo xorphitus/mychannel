d9d961145b4bff01999b247088f7d73787e13f58
T. Matsu <xorphitus@gmail.com>
2012-09-06 01:12:57 +0900

    カッコの付け方に統一感がなかったので修正

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -2,14 +2,14 @@ module Authentication
 
   class UnauthorizedError < StandardError; end
 
-  def self.included(base)
+  def self.included base
     base.before_filter :require_authentication
   end
 
   def require_authentication
     fb_auth = FbGraph::Auth.new(Settings.fb.app_id, Settings.fb.app_secret)
     begin
-      fb_auth.from_cookie(cookies)
+      fb_auth.from_cookie cookies
       @access_token = fb_auth.access_token
     rescue
       redirect_to root_url
