2c8ef158fc96da790d9532c3674eba6eb8f32532
T. Matsu <xorphitus@gmail.com>
2012-09-06 22:55:06 +0900

    リファクタ

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,10 +6,17 @@ require 'xmlrpc/client'
 class Array
   # get randan element of array
   def rand
-    if self.length == 0 then
+    if self.empty? then
       return nil
     end
     self[Kernel::rand self.length]
+  end
+end
+
+module Utils
+  def extract_keyphrases text
+    YaCan.appid = Settings.yahoo.app_id
+    YaCan::Keyphrase.extract post.message
   end
 end
 
@@ -18,17 +25,19 @@ end
 module TopicGenerator
   # ウォールの内容 (ランダム) とそこに含まれるキーワードに関連したニュースを取得
   def get_wall_and_news me
-    ret = []
-
     targets = me.home.select {|item| !item.message.nil?}
+    if targets.empty? then
+      return [({'text' => '最近はお友達のコメントもご無沙汰ですね'})]
+    end
+
+    ret = []
     post = targets.rand
     text = post.from().name + "さんからのポストです。\n" + post.message
 
     ret.push({'text' => text})
 
-    YaCan.appid = Settings.yahoo.app_id
-    k = YaCan::Keyphrase.extract post.message
-    if k.phrases.length > 0 then
+    k = Utils::extract_keyphrases post.message
+    unless k.phrases.empty? then
       keyphrase = k.phrases[0]
       ret.push({'text' => keyphrase + 'といえば'})
       rss = SimpleRSS.parse open("https://news.google.com/news/feeds?ned=us&ie=UTF-8&oe=UTF-8&q=#{URI.encode keyphrase}&lr&output=atom&num=5&hl=ja")
@@ -39,37 +48,37 @@ module TopicGenerator
         ret.push({'text' => 'なんてニュースがあるみたいです'})
       end
     end
+
     return ret
   end
 
   # 自分の投稿 (ランダム) とそこに含まれるキーワードから想起されるワードを取得
   def get_mypost_and_relatedword me
-    ret = []
-
     targets = me.feed.select {|item| !item.message.nil?}
-    if targets.length == 0 then
-      ret.push({'text' => '最近はFacebookへの投稿をしていないようですね'})
-    else
-      post = targets.rand
-      ret.push({'text' => '先日のあなたの投稿です'})
-      message = post.message
-      ret.push({'text' => message})
+    if targets.empty? then
+      return [({'text' => '最近はFacebookへの投稿をしていないようですね'})]
+    end
 
-      YaCan.appid = Settings.yahoo.app_id
-      k = YaCan::Keyphrase.extract message
-      if k.phrases.length > 0 then
-        keyphrase = k.phrases[0]
-        ret.push({'text' => keyphrase + 'といえば'})
-        server = XMLRPC::Client.new("d.hatena.ne.jp", "/xmlrpc")
-        result = server.call("hatena.getSimilarWord", {
-                               "wordlist" => [keyphrase]
-                             })
-        words = result['wordlist'].map {|v| v['word'] }
-        if words.length > 0 then
-          ret.push({'text' => words.rand + 'を思い浮かべますね'})
-        else
-          ret.push({'text' => '特に何も思い浮かびませんね'})
-        end
+    ret = []
+    post = targets.rand
+    ret.push({'text' => '先日のあなたの投稿です'})
+    message = post.message
+    ret.push({'text' => message})
+
+    # TODO ここは友達のリプライを取得して感情APIを叩くようにする
+    k = Utils::extract_keyphrases message
+    unless k.phrases.empty? then
+      keyphrase = k.phrases[0]
+      ret.push({'text' => keyphrase + 'といえば'})
+      server = XMLRPC::Client.new("d.hatena.ne.jp", "/xmlrpc")
+      result = server.call("hatena.getSimilarWord", {
+                             "wordlist" => [keyphrase]
+                           })
+      words = result['wordlist'].map {|v| v['word'] }
+      unless words.empty? then
+        ret.push({'text' => words.rand + 'を思い浮かべますね'})
+      else
+        ret.push({'text' => '特に何も思い浮かびませんね'})
       end
     end
 
@@ -81,7 +90,7 @@ module TopicGenerator
     ret = []
 
     music_list = me.music
-    if music_list.length == 0 then
+    if music_list.empty? then
       text = '音楽はあまりお好みではないですか?'
       return ret.push({'text' => text})
     end
