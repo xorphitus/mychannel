2e32c874025dfe009a2fb1dec2975f1c5db9368b
T. Matsu <xorphitus@gmail.com>
2012-09-26 23:27:10 +0900

    DashboardControllerのspecを記述

---
+++ spec/controllers/dashboard_controller_spec.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe DashboardController do
+  before do
+    class DashboardController
+      attr_accessor :channel_list
+
+      def get_fb_me
+        return nil
+      end
+    end
+
+    def Channel.find(arg0, arg1)
+      return []
+    end
+  end
+
+  describe "index" do
+    before do
+      @controller = DashboardController.new
+      @controller.index
+    end
+
+    it "assigns pairs of name and ID number to channel_list" do
+      @controller.channel_list.each do |channel|
+        channel.size.should == 2
+        channel[0].class.should == String
+        channel[1].class.should == Fixnum
+      end
+    end
+  end
+end
