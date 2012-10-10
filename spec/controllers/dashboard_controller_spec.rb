cef0f80e3d09afdd6c5f314eb2d8667c62e3c7da
T. Matsu <xorphitus@gmail.com>
2012-10-10 23:39:46 +0900

    エラー系のテストも追加

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,14 +2,6 @@
 require 'spec_helper'
 
 describe DashboardController do
-  before do
-    class DashboardController
-      def require_authentication
-        # 認証を外す / WebMockを使ってもきれいに解決できないため
-      end
-    end
-  end
-
   describe "index" do
     before do
       me_json = File.new(Rails.root.join("spec/webmocks/fb_me.json"))
@@ -17,19 +9,32 @@ describe DashboardController do
 
       user = Fabricate(:user)
       channel = Fabricate(:channel, user: user)
-
-      get "index"
     end
 
-    it "returns success response" do
-      response.should be_success
+    context "do not authenticated" do
+      before { get "index" }
+      it { response.should_not be_success }
     end
 
-    it "assigns pairs of name and ID number to channel_list" do
-      assigns[:channel_list].each do |channel|
-        channel.size.should == 2
-        channel[0].should be_an_instance_of String
-        channel[1].should be_an_instance_of Fixnum
+    context "authenticated" do
+      before do
+        class DashboardController
+          def require_authentication
+            # 認証を外す / WebMockを使ってもきれいに解決できないためやむなく再定義
+          end
+        end
+
+        get "index"
+      end
+
+      it { response.should be_success }
+
+      it "assigns pairs of name and ID number to channel_list" do
+        assigns[:channel_list].each do |channel|
+          channel.size.should == 2
+          channel[0].should be_an_instance_of String
+          channel[1].should be_an_instance_of Fixnum
+        end
       end
     end
   end
